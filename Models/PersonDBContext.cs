1851fcad719935927a747792461cfb5e6d0e899f
admin <admin@SOFTEDGEIT.COM>
2020-01-18 13:57:21 +0530

    Add project files.

---
+++ Models/PersonDBContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_EFCORE.Models
+{
+    public class PersonDBContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public PersonDBContext()
+        {
+
+        }
+
+        // Configure DB connection with Application
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb; Initial Catalog=PersonDb; Integrated Security=SSPI;");
+        }
+
+        // Provide mechanish of Model Mapping while database and tables are created
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Person>();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using CS_EFCORE.Models;
+using System;
+using System.Linq;
+
+namespace CS_EFCORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				ManageDatabase();
+
+				using (var ctx = new PersonDBContext())
+				{
+					var person = new Person()
+					{
+						PersonName = "Rajneesh",
+						Gender = "Male",
+						Age = 30						
+					};
+
+					ctx.Persons.Add(person);
+					ctx.SaveChanges();
+
+					Console.WriteLine("Added person");
+
+					foreach (var p in ctx.Persons.ToList())
+					{
+						// Use interpolation in C# 6.0
+						// This is equavalent to String builter. Mutable string object
+						Console.WriteLine($"{p.PersonId} {p.PersonName} {p.Age} {p.Gender}");
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"{ex.Message}");
+			}
+
+			Console.ReadLine();
+		}
+
+		// This method will make sure that if the database is already available, then delete it
+		static void ManageDatabase()
+		{
+			using (var ctx = new PersonDBContext())
+			{
+				ctx.Database.EnsureDeleted();
+				ctx.Database.EnsureCreated();
+			}
+		}
+    }
+}
